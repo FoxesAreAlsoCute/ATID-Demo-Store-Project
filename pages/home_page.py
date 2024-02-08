c685cd639bf55dd216991ec9abd041e4c1bd2a14
Shiian Sviatoslav <fxsrlsct@gmail.com>
2024-02-08 19:11:55 +0100

    Add smoke method

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,4 +1,10 @@
 from .base_page import BasePage
+from ._locators import HomePageLocators
 
 class HomePage(BasePage):
-    pass
+    expected_text="ATID Demo Store"
+    
+    def should_be_home_page(self):
+        assert self.element_is_not_visible(HomePageLocators.MAIN_TITLE), "Main title is not visible"
+        text = self.get_visible_text(HomePageLocators.MAIN_TITLE)
+        assert text == self.expected_text, "Text of main title is not as expected"
