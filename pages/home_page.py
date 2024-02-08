152483263b2dde7e2d7354bb14fe82cf337a6e0a
Shiian Sviatoslav <fxsrlsct@gmail.com>
2024-02-08 19:31:13 +0100

    Add smoke test

--- pages/home_page.py
+++ pages/home_page.py
@@ -5,6 +5,6 @@ class HomePage(BasePage):
     expected_text="ATID Demo Store"
     
     def should_be_home_page(self):
-        assert self.element_is_not_visible(HomePageLocators.MAIN_TITLE), "Main title is not visible"
+        assert self.element_is_visible(HomePageLocators.MAIN_TITLE), "Main title is not visible"
         text = self.get_visible_text(HomePageLocators.MAIN_TITLE)
         assert text == self.expected_text, "Text of main title is not as expected"
